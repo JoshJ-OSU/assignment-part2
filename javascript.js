6f285e48afb13021c0a6ce0ae715826e8ea609cc
Joshua Johnson <johnsjo3@flip1.engr.oregonstate.edu>
2015-04-24 00:27:19 -0700

    conformed JavaScript to jslint standards.  Conformed html to w3c standards.

--- javascript.js
+++ javascript.js
@@ -1,37 +1,22 @@
+/*jslint browser:true */
+
 var MIN_PAGES = 1;
 var MAX_PAGES = 5;
 
-var favGists = [];   //favorite gists (stored in localStorage) - see window.onload
+var favGists = [];   //favorite gists (stored in localStorage)
 var resultGists = []; //stores results after calling fetchGists
-
-//Called from html, calls AJAX function getGists and loads resultGists 
-function initiateSearch() {  
-  //loop calls for gists, parses them, and pushes them to the array results
-  var pages = document.getElementsByName('number-of-pages')[0].value;
-  if ((pages < MIN_PAGES) || (pages > MAX_PAGES)) {
-    var str = "You must select from 1 to 5 pages";
-    document.getElementById('error-message').innerText = str;
-  }
-  else {
-    document.getElementById('error-message').innerText = "";
-    for (var i = 0; i < pages; i++) {
-      var page = i+1;
-      getGists(page);
-    }
-  }
-}
 
 
 //Gist object
-function Gist(id,description,html_url,fileLanguages) {
-  this.id = id
+function Gist(id, description, html_url, fileLanguages) {
+  this.id = id;
   this.description = description;
   this.html_url = html_url;
   this.fileLanguages = fileLanguages;
-  this.containsLanguage = function(l) {
+  this.containsLanguage = function (l) {
     var result = false;
-    fileLanguages.forEach(function(s){
-      if (result === true || s === l){
+    fileLanguages.forEach(function (s) {
+      if (result === true || s === l) {
         result = true;
       }
     });
@@ -39,147 +24,132 @@ function Gist(id,description,html_url,fileLanguages) {
   };
 }
 
-/*
-function lFilter() = {
-  this->python = document.getElementById('lfilter-python').checked;
-  json : document.getElementById('lfilter-json').checked;
-  js : document.getElementById('lfilter-javascript').checked;
-  sql : document.getElementById('lfilter-sql').checked;
-  unchecked : function(){
-    if (!python && !json && !js && !sql) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-};
-*/
 
-/* Requests gists from github.  The following parameters are used:
-  URL
-*/
-function getGists(page){
-  //clear results off page
-  while (resultGists.length > 0){
-    var id = resultGists[0].id;
-    var fromTbl = document.getElementById('search-gist-results');
-    var rowToRemove = document.getElementById(id);
-    fromTbl.removeChild(rowToRemove);
-    removeResultGist(0);
-  }
-  
-  //get filters from html page
-  var lFilter = [];
-  if (document.getElementsByName('lfilter-python')[0].checked === true){
-    lFilter.push("Python");
-  }
-  if (document.getElementsByName('lfilter-json')[0].checked === true){
-    lFilter.push("JSON");
-  }
-  if (document.getElementsByName('lfilter-javascript')[0].checked === true){
-    lFilter.push("JavaScript");
-  }
-  if (document.getElementsByName('lfilter-sql')[0].checked === true){
-    lFilter.push("SQL");
-  }  
-  
-  //create request
-  var req = new XMLHttpRequest();
-  if(!req){
-    throw 'Unable to create HttpRequest.';
-  }
-  var url = 'https://api.github.com/gists/public';
-  //used in debugging
-  //var url = 'http://web.engr.oregonstate.edu/~johnsjo3/CS290/assign3p2/localGists';
-    req.onreadystatechange = function(){
-      if(this.readyState === 4){
-        //var gists stores parsed response text
-        var parsedGistPage = JSON.parse(this.responseText);    
-        
-        parsedGistPage.forEach(function(s){
-          var id = s.id;
-          var html_url = s.html_url;
-          var description = s.description;
-          if (description.length === 0){
-            description = "No Description"
-          }
-          
-          var fileLanguages = [];
-          
-          //adapted from instructor's code (Piazza @180)
-          //pulls all languages used in gist and puts them into an array
-          for (var property in s.files) {
-            if (s.files.hasOwnProperty(property)) {
-              if (s.files[property].language){
-                fileLanguages.push(s.files[property].language);
-              }
-            }
-          }
-          
-          newGist = new Gist(id,description,html_url,fileLanguages);
-          var passTest = false;
-          if(lFilter.length === 0) {
-            passTest = true;
-          }
-          else {
-            lFilter.forEach(function(f){
-              if (newGist.containsLanguage(f) === true) {
-                passTest = true;
-              }
-            });
-          }
-          if (passTest === true) {
-            addResultGist(newGist);
-          }
-        });
-      }
-    }
-  req.open('GET', url  + '?page=' + (page));
-  req.send();
+//prints the passed gist object to screen
+function printResultGist(g) {
+  var tblBody = document.getElementById('search-gist-results');
+  var row = document.createElement('tr');
+  row.setAttribute("id", g.id);
+
+  //button
+  var cell1 = document.createElement('td');
+  var button = document.createElement('input');
+  button.setAttribute("type", "button");
+  button.setAttribute("value", "Move to Favorites");
+  button.setAttribute("onclick", "moveGistToFav('" + g.id + "')");
+  cell1.appendChild(button);
+
+  //description and html link
+  var cell2 = document.createElement('td');
+  var link = document.createElement('a');
+  link.setAttribute("href", g.html_url);
+  link.setAttribute("target", "_blank");
+  link.innerText = g.description;
+  cell2.appendChild(link);
+
+  //bulleted list of languages
+  var cell3 = document.createElement('td');
+  var ul = document.createElement('ul');
+  g.fileLanguages.forEach(function (l) {
+    var bullet = document.createElement('li');
+    bullet.innerText = l;
+    ul.appendChild(bullet);
+  });
+  cell3.appendChild(ul);
+
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+
+  tblBody.appendChild(row);
 }
+
+
+//prints passed gist object to screen
+function printFavGist(g) {
+  var tblBody = document.getElementById('favorite-gists');
+  var row = document.createElement('tr');
+  row.setAttribute("id", g.id);
+
+  //button
+  var cell1 = document.createElement('td');
+  var button = document.createElement('input');
+  button.setAttribute("type", "button");
+  button.setAttribute("value", "Remove");
+  button.setAttribute("onclick", "moveGistToResult('" + g.id + "')");
+  cell1.appendChild(button);
+
+  //description and html link
+  var cell2 = document.createElement('td');
+  var link = document.createElement('a');
+  link.setAttribute("href", g.html_url);
+  link.setAttribute("target", "_blank");
+  link.innerText = g.description;
+  cell2.appendChild(link);
+
+  //bulleted list of languages
+  var cell3 = document.createElement('td');
+  var ul = document.createElement('ul');
+  g.fileLanguages.forEach(function (l) {
+    var bullet = document.createElement('li');
+    bullet.innerText = l;
+    ul.appendChild(bullet);
+  });
+  cell3.appendChild(ul);
+
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+
+  tblBody.appendChild(row);
+}
+
+
 
 //adds a passed gist to the resultGists array and prints to html page
 function addResultGist(g) {
   //make sure gist is not already in favorites
   var inFavs = false;
-  favGists.forEach(function(f){
-      inFavs = (inFavs || (g.id === f.id));
+  favGists.forEach(function (f) {
+    inFavs = (inFavs || (g.id === f.id));
   });
-  
+
   //make sure gist is not already in results
   var inResults = false;
-  resultGists.forEach(function(f){
+  resultGists.forEach(function (f) {
     inResults = (inResults || (g.id === f.id));
   });
 
-  if (inFavs === false && inResults === false){
+  if (inFavs === false && inResults === false) {
     //add element to the array and print
     resultGists.push(g);
     printResultGist(g);
   }
 }
 
+
 //removes index from array and removes corresponding element from html page
 function removeResultGist(index) {
   //removes element from array
-  resultGists.splice(index, 1);    
+  resultGists.splice(index, 1);
 }
 
+
+//adds to favGists, updates localStorage, and prints to screen
 function addFavGist(g) {
   //make sure gist is not already in favorites
   var inFavs = false;
-  favGists.forEach(function(f){
-      inFavs = (inFavs || (g.id === f.id));
+  favGists.forEach(function (f) {
+    inFavs = (inFavs || (g.id === f.id));
   });
-  
+
   //make sure gist is not already in results
   var inResults = false;
-  resultGists.forEach(function(f){
+  resultGists.forEach(function (f) {
     inResults = (inResults || (g.id === f.id));
   });
 
-  if (inFavs === false && inResults === false){
+  if (inFavs === false && inResults === false) {
     //update FavGist array
     favGists.push(g);
     //update localStorage
@@ -189,85 +159,21 @@ function addFavGist(g) {
   }
 }
 
+
+//removes from favGists, updates localStorage
 function removeFavGist(index) {
   //removes element from array
-  favGists.splice(index, 1); 
+  favGists.splice(index, 1);
   //update localStorage
   localStorage.setItem('favGists-storage', JSON.stringify(favGists));
 }
 
-function printResultGist(g){
-  var tblBody = document.getElementById('search-gist-results');
-  var row = document.createElement('tr');
-  row.setAttribute("id", g.id)
-  
-  //button
-  var cell1 = document.createElement('td');
-  var button = document.createElement('input');
-  button.setAttribute("type","button")
-  button.setAttribute("value", "Move to Favorites");
-  button.setAttribute("onclick","moveGistToFav('" + g.id + "')");
-  cell1.appendChild(button);
-  
-  //description and html link
-  var cell2 = document.createElement('td');
-  cell2.innerHTML = "<a href=\'" + g.html_url + "\'>" + g.description + "</a>";
-  
-  //bulleted list of languages
-  var cell3 = document.createElement('td');
-  var ul = document.createElement('ul');
-  g.fileLanguages.forEach(function(l){
-    var bullet = document.createElement('li');
-    bullet.innerText = l;
-    ul.appendChild(bullet);
-  });
-  cell3.appendChild(ul);
-  
-  row.appendChild(cell1);
-  row.appendChild(cell2);
-  row.appendChild(cell3);
-  
-  tblBody.appendChild(row);
-}
 
-function printFavGist(g){
-  var tblBody = document.getElementById('favorite-gists');
-  var row = document.createElement('tr');
-  row.setAttribute("id", g.id)
-  
-  //button
-  var cell1 = document.createElement('td');
-  var button = document.createElement('input');
-  button.setAttribute("type","button")
-  button.setAttribute("value", "Remove");
-  button.setAttribute("onclick","moveGistToResult('" + g.id + "')");
-  cell1.appendChild(button);
-  
-  //description and html link
-  var cell2 = document.createElement('td');
-  cell2.innerHTML = "<a href=\'" + g.html_url + "\'>" + g.description + "</a>";
-  
-  //bulleted list of languages
-  var cell3 = document.createElement('td');
-  var ul = document.createElement('ul');
-  g.fileLanguages.forEach(function(l){
-    var bullet = document.createElement('li');
-    bullet.innerText = l;
-    ul.appendChild(bullet);
-  });
-  cell3.appendChild(ul);
-  
-  row.appendChild(cell1);
-  row.appendChild(cell2);
-  row.appendChild(cell3);
-  
-  tblBody.appendChild(row);
-}
 
+
+
+//moves gist from results to fav section
 function moveGistToFav(id) {
-  //for debugging
-  console.log(id);
-  
   //remove element from page
   var fromTbl = document.getElementById('search-gist-results');
   var rowToMove = document.getElementById(id);
@@ -275,23 +181,22 @@ function moveGistToFav(id) {
 
   //find element in array
   var index;
-  for (var i = 0; i < resultGists.length; i++){
+  var i;
+  for (i = 0; i < resultGists.length; i++) {
     if (resultGists[i].id === id) {
       index = i;
-      console.log(index);
     }
   }
-  
-  tmp = resultGists[index];
+
+  var tmp = resultGists[index];
   removeResultGist(index);
   addFavGist(tmp);
 
 }
 
+
+//moves gist from fav section to results section
 function moveGistToResult(id) {
-  //for debugging
-  console.log(id);
-  
   //remove element from page
   var fromTbl = document.getElementById('favorite-gists');
   var rowToMove = document.getElementById(id);
@@ -299,30 +204,144 @@ function moveGistToResult(id) {
 
   //find element in array
   var index;
-  for (var i = 0; i < favGists.length; i++){
+  var i;
+  for (i = 0; i < favGists.length; i++) {
     if (favGists[i].id === id) {
       index = i;
     }
   }
 
-  tmp = favGists[index];
-  removeFavGist(index);  
+  var tmp = favGists[index];
+  removeFavGist(index);
   addResultGist(tmp);
 
 }
 
-//Loads user favorites on load
-window.onload = function() {
-  var favStr = localStorage.getItem('favGists-storage');
-  if( favStr === null ) {
-    localStorage.setItem('favGists-storage', JSON.stringify(favGists));
+
+function getGists(page) {
+  //clear results off page
+  var gid;
+  var fromTbl;
+  var rowToRemove;
+  while (resultGists.length > 0) {
+    gid = resultGists[0].id;
+    fromTbl = document.getElementById('search-gist-results');
+    rowToRemove = document.getElementById(gid);
+    fromTbl.removeChild(rowToRemove);
+    removeResultGist(0);
   }
-  else {
+
+  //get filters from html page
+  var lFilter = [];
+  if (document.getElementsByName('lfilter-python')[0].checked === true) {
+    lFilter.push("Python");
+  }
+  if (document.getElementsByName('lfilter-json')[0].checked === true) {
+    lFilter.push("JSON");
+  }
+  if (document.getElementsByName('lfilter-javascript')[0].checked === true) {
+    lFilter.push("JavaScript");
+  }
+  if (document.getElementsByName('lfilter-sql')[0].checked === true) {
+    lFilter.push("SQL");
+  }
+
+  //create request
+  var req = new XMLHttpRequest();
+  if (!req) {
+    throw 'Unable to create HttpRequest.';
+  }
+  var url = 'https://api.github.com/gists/public';
+  //used in debugging
+  //var url = 
+  //  'http://web.engr.oregonstate.edu/~johnsjo3/CS290/assign3p2/localGists';
+
+  req.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      //var gists stores parsed response text
+      var parsedGistPage = JSON.parse(this.responseText);
+
+      /*Each parsed element is put into a gist object and passed through
+      * a filter before being added to resultGists and printed to screen
+      */
+      parsedGistPage.forEach(function (s) {
+        var id = s.id;
+        var html_url = s.html_url;
+        var description = s.description;
+        if (description === null || description.length === 0) {
+          description = "No Description";
+        }
+
+        //adapted from instructor's code (Piazza @180)
+        //pulls all languages used in gist and puts them into an array
+        var fileLanguages = [];
+        var property;
+        for (property in s.files) {
+          if (s.files.hasOwnProperty(property)) {
+            if (s.files[property].language) {
+              fileLanguages.push(s.files[property].language);
+            }
+          }
+        }
+
+        //create the gist object
+        var newGist = new Gist(id, description, html_url, fileLanguages);
+
+        //test to see if gist passes the filter tests
+        var passTest = false;
+        if (lFilter.length === 0) {
+          passTest = true;
+        } else {
+          lFilter.forEach(function (f) {
+            if (newGist.containsLanguage(f) === true) {
+              passTest = true;
+            }
+          });
+        }
+
+        //if gist passes, add to resultGist and print to screen
+        if (passTest === true) {
+          addResultGist(newGist);
+        }
+      });
+    }
+  };
+  req.open('GET', url  + '?page=' + page);
+  req.send();
+}
+
+
+//Called from html, calls AJAX function getGists and loads resultGists 
+function initiateSearch() {
+  //loop calls for gists, parses them, and pushes them to the array results
+  var pages = document.getElementsByName('number-of-pages')[0].value;
+  if ((pages < MIN_PAGES) || (pages > MAX_PAGES)) {
+    var str = "";
+    str = "You must select from " + MIN_PAGES + " to " + MAX_PAGES + " pages.";
+    document.getElementById('error-message').innerText = str;
+  } else {
+    document.getElementById('error-message').innerText = "";
+
+    var i;
+    var page;
+    for (i = 0; i < pages; i++) {
+      page = i + 1;
+      getGists(page);
+    }
+  }
+}
+
+
+//Loads user favorites on load
+window.onload = function () {
+  var favStr = localStorage.getItem('favGists-storage');
+  if (favStr === null) {
+    localStorage.setItem('favGists-storage', JSON.stringify(favGists));
+  } else {
     favGists = JSON.parse(localStorage.getItem('favGists-storage'));
   }
-  
-  favGists.forEach(function(g){
+
+  favGists.forEach(function (g) {
     printFavGist(g);
   });
-  
-}
+};
